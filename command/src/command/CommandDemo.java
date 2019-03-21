d0542a85e8d493904fd092852b0b3cabfb28758a
ionc21 <ionc86@gmail.com>
2019-03-21 09:45:44 +0100

    interpreter

--- command/src/command/CommandDemo.java
+++ command/src/command/CommandDemo.java
@@ -6,22 +6,22 @@ import java.util.List;
 //client
 public class CommandDemo {
 
-    public static void main(String args[]) {
-        Light bedroomLight = new Light();
-        Light kitchenLight = new Light();
-        Switch lightSwitch = new Switch();
+	public static void main(String args[]) {
+		Light bedroomLight = new Light();
+		Light kitchenLight = new Light();
+		Switch lightSwitch = new Switch();
 
-        Command toggleCommand = new ToggleCommand(bedroomLight);
+		Command toggleCommand = new ToggleCommand(bedroomLight);
 
-        lightSwitch.storeAndExecute(toggleCommand);
-        //lightSwitch.storeAndExecute(toggleCommand);
-        //lightSwitch.storeAndExecute(toggleCommand);
+		lightSwitch.storeAndExecute(toggleCommand);
+		// lightSwitch.storeAndExecute(toggleCommand);
+		// lightSwitch.storeAndExecute(toggleCommand);
 
-        List<Light> lights = new ArrayList<>();
-        lights.add(kitchenLight);
-        lights.add(bedroomLight);
-        Command allLightsCommand = new AllLightsCommand(lights);
+		List<Light> lights = new ArrayList<>();
+		lights.add(kitchenLight);
+		lights.add(bedroomLight);
+		Command allLightsCommand = new AllLightsCommand(lights);
 
-        lightSwitch.storeAndExecute(allLightsCommand);
-    }
+		lightSwitch.storeAndExecute(allLightsCommand);
+	}
 }
